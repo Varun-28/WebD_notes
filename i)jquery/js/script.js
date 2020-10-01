8e1e3f8b3fd87d9afb38f2adb2abbf854da49c42
Varun Verma <28varun28pp@gmail.com>
2020-10-01 23:55:39 +0530

    1/10/2020

---
+++ i)jquery/js/script.js
@@ -0,0 +1,54 @@
+//script.js
+
+//jquery begins
+$(function() {
+
+//1. install.html part
+    
+    //alert("hello!!");
+    
+//2. syntax.html part
+    
+    //we select html elements in jquery and perform action on it as:-
+    //$(selector).action();
+
+    //$('#box').hide();
+    
+    //$('.thing').fadeOut();
+    
+    //$('h1').css("color","blue");
+
+    //$("button").click(function() {
+    // $('#box').fadeOut(1000);
+    //});
+
+//3. selectors.html part
+    
+    // Grouping selector
+    // $('h2,h3,h4').css('border','solid 1px red');
+    
+    // ID selector
+    // $('div#container').css('border','solid 1px red');
+    
+    // Class selector
+    // $('p.lead').css('border','solid 1px red');
+    
+    // Pseudo-element selector - selects first element
+    // $('li:first').css('border','solid 1px red');
+    
+    // Pseudo-element selector - selects even P tags
+    // $('p:even').css('border','solid 1px red');
+    
+    // Descendant selector
+    // $('div em').css('border','solid 1px red');
+    
+    // Child selector
+    // $('div > p').css('border','solid 1px red');
+    
+    // jQuery Header selector i.e.; h1,h2,h3...h6
+    // $(':header').css('border','solid 1px red');
+    
+    // jQuery Contains selector
+    // $('div:contains("Brad")').css('border','solid 1px red');
+    
+});
